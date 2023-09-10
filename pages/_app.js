3f61655f2a8f52b0f0fc28cb25fcc363acd4d4c3
Nils Holthey <nils.holthey@gmx.de>
2023-09-10 12:06:35 +0200

    remove font from _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import GlobalStyles from '@/styles/GlobalStyles';
 
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function App({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
+    <>
       <GlobalStyles />
       <Component {...pageProps} />
-    </main>
+    </>
   );
 }
