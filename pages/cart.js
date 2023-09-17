daa67e204b11ccee9d9dfc9dd0c078c45a3a736b
Nils Holthey <nils.holthey@gmx.de>
2023-09-17 14:30:07 +0200

    add cart api

--- pages/cart.js
+++ pages/cart.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Button from '@/components/Button';
 import { CartContext } from '@/components/CartContext';
 import Layout from '@/components/Layout';
